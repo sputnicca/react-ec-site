e914e3c288ccb7544082b59cca2ce5661f0454be
sputnicca <ananas27x@gmail.com>
2022-04-26 17:41:53 +0900

    ログイン機能のレイアウト・スタイルを決める

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import '../css/app.scss'
+import '../css/style.scss'
 
 class Login extends React.Component {
     render() {
         return (
             <>
-              <p>login</p>
+              <div className="login-wrapper">
+                <form className="box login-box">
+                  <div className="field">
+                      <label className="label">Email</label>
+                      <div className="control">
+                        <input className="input" type="text" placeholder="メールアドレス" />
+                      </div>
+                  </div>
+                  <div className="field">
+                      <label className="label">Password</label>
+                      <div className="control">
+                        <input className="input" type="password" placeholder="パスワード" />
+                      </div>
+                  </div>
+                  <div className="control">
+                      <button className="button is-fullwidth is-primary">Login</button>
+                  </div>
+                </form>
+              </div>
             </>
         )
     }
