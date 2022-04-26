6d41bcdafe37489389e803e8804ffd804dbad21f
sputnicca <ananas27x@gmail.com>
2022-04-26 14:46:08 +0900

    初期化:コンポーネント分割

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Login extends React.Component {
+    render() {
+        return (
+            <>
+              <p>login</p>
+            </>
+        )
+    }
+}
+
+export default Login
