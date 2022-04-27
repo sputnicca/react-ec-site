12e0975aee26c83754df0500a609781404aa01cf
sputnicca <ananas27x@gmail.com>
2022-04-27 15:21:16 +0900

    スタイルシートのモジュール分割/dart-sass

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+              <p>Header component</p>
+            </div>
+        )
+    }
+}
+
+export default Header
