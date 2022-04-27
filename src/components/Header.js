0e2c49a340a4afacaec339359ebeb068dd3c83d5
sputnicca <ananas27x@gmail.com>
2022-04-27 20:10:47 +0900

    クラス式→関数式

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,79 @@
 import React from 'react'
 
-class Header extends React.Component {
-    renderLink() {
-        const nickname = this.props.nickname
-        if(nickname){
-            return (
-                <span className="nickname">
-                  {/* this=Headerコンポーネント */}
-                  {this.props.nickname}
-                </span>
+const Header = props => {
+  return (
+    <div className="header">
+      <div className="grid">
+        <div className="start">
+          <a href="/">HOME</a>
+        </div>
+        <div className="end">
+          {/* {this.renderLink()} */}
+          {props.nickname ? (
+            <span className="nickname">
+              <i className="far fa-user"></i>
+                {props.nickname}
+            </span>
+            ) :
+            (
+              <>
+                <a href="/">ログイン</a>
+                <a href="/">登録</a>
+              </>
             )
-        } else {
-            return (
-                <>
-                  <a href="/">ログイン</a>
-                  <a href="/">登録</a>
-                </>
-            )
-        }
-    }
-    render() {
-        return (
-            <div className="header">
-                <div className="grid">
-                    <div className="start">
-                        <a href="/">HOME</a>
-                    </div>
-                    <div className="end">
-                        {this.renderLink()}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+          }
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Header
+// class Header extends React.Component {
+    // renderLink() {
+    //     const nickname = this.props.nickname
+    //     if(nickname){
+    //         return (
+    //             <span className="nickname">
+    //               <i className="far fa-user"></i>
+    //               {/* this=Headerコンポーネント */}
+    //               {this.props.nickname}
+    //             </span>
+    //         )
+    //     } else {
+    //         return (
+    //             <>
+    //               <a href="/">ログイン</a>
+    //               <a href="/">登録</a>
+    //             </>
+    //         )
+    //     }
+    // }
+//     render() {
+//         return (
+//             <div className="header">
+//                 <div className="grid">
+//                     <div className="start">
+//                         <a href="/">HOME</a>
+//                     </div>
+//                     <div className="end">
+//                         {/* {this.renderLink()} */}
+//                         {this.props.nickname ? (
+//                             <span className="nickname">
+//                                 <i className="far fa-user"></i>
+//                                 {this.props.nickname}
+//                             </span>
+//                         ) :
+//                         (
+//                             <>
+//                               <a href="/">ログイン</a>
+//                               <a href="/">登録</a>
+//                             </>
+//                         )
+//                         }
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
