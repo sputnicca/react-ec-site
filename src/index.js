15b0669be2748529d5daaf342b716c0dad6eaa98
sputnicca <ananas27x@gmail.com>
2022-04-27 21:24:59 +0900

    記法の調整・コンソールエラー解消

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ReactDOM from 'react-dom'
 import 'css/style.scss'
 import App from 'components/App'
 import Login from 'components/Login'
 import NotFound from 'components/NotFound'
+import { createRoot } from "react-dom/client"
 
-ReactDOM.render(
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement)
+
+root.render(
   <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<App />} />
@@ -14,5 +17,4 @@ ReactDOM.render(
       <Route element={<NotFound />} />
     </Routes>
   </BrowserRouter>,
-  document.getElementById('root')
-);
+)
