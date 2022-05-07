7a0800fc354b1bfeebeeaac4d8164f913bfc3407
sputnicca <ananas27x@gmail.com>
2022-05-07 18:55:45 +0900

    商品表示のレイアウト

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class NavBar extends React.Component {
+    render() {
+        return (
+          <div className="nav-bar">
+            <p>NavBar</p>
+          </div>
+        )
+    }
+}
+
+export default NavBar
---
+++ src/components/ProductItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ProductItem extends React.Component {
+    render() {
+        return (
+          <div className="product-item">
+            <p>Product Item</p>
+          </div>
+        )
+    }
+}
+
+export default ProductItem
